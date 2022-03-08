37bf9fef9adca497eed395242415c199c5820fd2
ddoyediran <ddoyediran@gmail.com>
2022-03-08 20:34:09 +0100

    BST updated

--- trees.js
+++ trees.js
@@ -8,14 +8,20 @@
  * 2.) Parent(s): Are Node that has a child or children.
  * 3.) Siblings: Are children or nodes that has same parent.
  * 4.) Leaf: Are node(s) without a child or children.
- * 5.) Depth of x: Is the length of path from root to node x. Depth of root node is 0.
+ * 5.) Depth of x: Is the length of path from root to node x. Depth of root node is 0
  * 6.) Height of x: Number of edges or length in the longest path from the root to Node x.
+ * 7.) Height of the overall trees - it is the height of the trees.
  *
  * Applications: storing hierarchical data (e.g File system), organize data (for quick search, insertation, deletion e.g Binary Search Tree), Tries (dictionaries), Network Routing Algorigthm
  * 
  * Binary Search Trees, BST
  * BST is a binary tree in which for each node, the value of all the nodes in the right subtreeof the nodes are greater
  * and the value of all the nodes in the left subtree are lesser or equal.
+ * 
+ * // Rules to keep note
+ * a.) BT must have atleast 2 children
+ * b.) Must have exactly one root
+ * c.) Exactly one path between root and any node.
  */
 
 // Implementation
@@ -25,5 +31,28 @@ class BinarySearchNode {
     this.data = data;
     this.left = left;
     this.right = right;
+  }
+}
+
+let newNode = new BinarySearchNode(data);
+
+newNode;
+
+function getNewNode(data) {
+  let newNode = new BinarySearchNode(data);
+  newNode.data = data;
+  newNode.left = null;
+  newNode.right = null;
+
+  return newNode;
+}
+
+class BinaryTree {
+  contructor() {}
+
+  insert(root, data) {
+    if (root === null) {
+      root = getNewNode();
+    }
   }
 }
